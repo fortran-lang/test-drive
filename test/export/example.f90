2d496662b38a2501b79e69a8b670945b1f732a6c
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2026-03-27 10:22:58 +0100

    Update CMake configuration, allow meson to emit CMake config files (#61)

---
+++ test/export/example.f90
@@ -0,0 +1,23 @@
+program tester
+  use, intrinsic :: iso_fortran_env, only : error_unit
+  use testdrive, only : run_testsuite, new_testsuite, testsuite_type
+  implicit none
+  integer :: stat, is
+  type(testsuite_type), allocatable :: testsuites(:)
+  character(len=*), parameter :: fmt = '("#", *(1x, a))'
+
+  stat = 0
+
+  allocate(testsuites(0))
+
+  do is = 1, size(testsuites)
+    write(error_unit, fmt) "Testing:", testsuites(is)%name
+    call run_testsuite(testsuites(is)%collect, error_unit, stat)
+  end do
+
+  if (stat > 0) then
+    write(error_unit, '(i0, 1x, a)') stat, "test(s) failed!"
+    error stop
+  end if
+
+end program tester
